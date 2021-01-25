006364e252a07bc96be02d79069c1e850d59514a
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-01-25 12:13:57 +0900

    signup
    signup

--- app/src/main/java/com/example/postit/view/SignUpActivity.kt
+++ app/src/main/java/com/example/postit/view/SignUpActivity.kt
@@ -8,7 +8,6 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProvider
 import com.example.postit.R
 import com.example.postit.network.model.Req
@@ -16,11 +15,10 @@ import com.example.postit.repository.AppRepo
 import com.example.postit.viewmodel.LoginVM
 import com.example.postit.viewmodel.ViewModelProviderFactory
 import kotlinx.android.synthetic.main.activity_sign_up.*
-import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
     private var idChk = false
-    private val chk = 4..15
+    private val range = 4..15
     private lateinit var id: EditText
     private lateinit var pw: EditText
     private lateinit var pw2: EditText
@@ -38,8 +36,12 @@ class SignUpActivity : AppCompatActivity() {
             } else Toast.makeText(this, "사용 불가능한 id 입니다.", Toast.LENGTH_SHORT).show()
         })
         VM.singUpRes.observe(this, androidx.lifecycle.Observer {
-            if (it.result==1)
-            sign_up_pb.visibility=View.INVISIBLE
+            if (it.result == 1) {
+                finish()
+                Toast.makeText(this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show()
+            }
+            else Toast.makeText(this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show()
+
         })
         sign_up_edt_id.editText?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -78,23 +80,24 @@ class SignUpActivity : AppCompatActivity() {
 
 
     fun check(): Boolean {
-        Log.d("TAG", id.toString())
-        Log.d("TAG", pw.toString())
-        Log.d("TAG", pw2.toString())
-        return if (pw.text.toString() != pw2.text.toString()) {
+        var chk=true
+        if (pw.text.toString() != pw2.text.toString()) {
             Toast.makeText(this, "비밀번호가 서로 같지 않습니다", Toast.LENGTH_SHORT).show()
-            false
-        } else {
-            return if (id.text.toString().length in chk && pw.text.toString().length in chk) true
-            else {
-                Toast.makeText(this, "ID 혹은 비밀번호를 확인하세요", Toast.LENGTH_SHORT).show()
-                false
-            }
+            chk=false
         }
+        if (id.text.toString().length !in range || pw.text.toString().length !in range){
+            Toast.makeText(this, "id 또는 비밀번호가 형식에 맞지 않습니다", Toast.LENGTH_SHORT).show()
+            chk=false
+        }
+        if (!sign_up_radio_female.isChecked && !sign_up_radio_male.isChecked){
+            Toast.makeText(this, "성별을 체크해주세요", Toast.LENGTH_SHORT).show()
+            chk=false
+        }
+        return chk
     }
 
     fun chkId(view: View) {
-        if (id.text.toString().length in chk) VM.chkId(id.text.toString())
+        if (id.text.toString().length in range) VM.chkId(id.text.toString())
         else Toast.makeText(this, "형식에 맞게 입력해주세요", Toast.LENGTH_SHORT).show()
     }
 
