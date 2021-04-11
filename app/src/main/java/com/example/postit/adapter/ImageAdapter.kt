f14fb372f438e23d00d4c2ad26b7f0e0795ccd42
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-04-11 19:59:14 +0900

    PostActivity

--- app/src/main/java/com/example/postit/adapter/ImageAdapter.kt
+++ app/src/main/java/com/example/postit/adapter/ImageAdapter.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.postit.R
@@ -17,9 +19,16 @@ class ImageAdapter(val imgList: ArrayList<String>, val VIEW_TYPE: Boolean) :
 
     inner class ImageViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         lateinit var imgItem: ImageView
-        fun bind() {
+        fun bind(img: String) {
             if (VIEW_TYPE) imgItem = view.findViewById(R.id.img_rv_item_image)
-            else imgItem = view.findViewById(R.id.img_rv_item_img_post)
+            else {
+                val btnRemove=view.findViewById<ImageButton>(R.id.btn_rv_item_remove)
+                imgItem = view.findViewById(R.id.img_rv_item_img_post)
+                btnRemove.setOnClickListener {
+                    deleteAtList(img)
+                    Toast.makeText(context, "이미지가 삭제되었습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
             Glide.with(context)
                 .load(imgList[adapterPosition])
                 .centerCrop()
@@ -37,13 +46,14 @@ class ImageAdapter(val imgList: ArrayList<String>, val VIEW_TYPE: Boolean) :
         viewType: Int
     ): ImageAdapter.ImageViewHolder {
         context = parent.context
-        if (VIEW_TYPE){
+        if (VIEW_TYPE) {
             return ImageViewHolder(
                 LayoutInflater.from(parent.context).inflate(R.layout.rv_image_item, parent, false)
             )
-        }else{
+        } else {
             return ImageViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.rv_img_item_post,parent,false)
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.rv_img_item_post, parent, false)
             )
         }
 
@@ -54,12 +64,17 @@ class ImageAdapter(val imgList: ArrayList<String>, val VIEW_TYPE: Boolean) :
     }
 
     override fun onBindViewHolder(holder: ImageAdapter.ImageViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(imgList[position])
     }
 
     fun setList(imgList: ArrayList<String>) {
         this.imgList.addAll(imgList)
-        Log.d("postit",this.imgList.toString())
+        Log.d("postit", this.imgList.toString())
+        notifyDataSetChanged()
+    }
+
+    fun deleteAtList(img: String) {
+        imgList.remove(img)
         notifyDataSetChanged()
     }
 }
